54e97a7bcdf16bb0e25dead34b4815b14d721d44
Daniel Campuzano <yesdanlocooo@gmail.com>
2025-07-19 22:15:37 -0300

    pixel changes

--- app/layout.js
+++ app/layout.js
@@ -75,7 +75,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '4168648746699090');
+fbq('init', '629254799619654');
 fbq('track', 'PageView');`}
         </script>
         <noscript>
